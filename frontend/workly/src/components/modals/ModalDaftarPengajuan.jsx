46bfb0f1e1919d8c3a0c2ce698ee064319f116fc
HadiNasution <hadinasution26@gmail.com>
2023-12-21 21:10:47 +0700

    create recap persmission

--- frontend/workly/src/components/modals/ModalDaftarPengajuan.jsx
+++ frontend/workly/src/components/modals/ModalDaftarPengajuan.jsx
@@ -1,8 +1,12 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Swal from "sweetalert2";
+import { convertDayString } from "../../utils/date-time";
+import { BsImageFill, BsClockFill } from "react-icons/bs";
 
 export default function ModalDaftarPengajuan() {
+  const [recap, setRecap] = useState(null);
+
   const getDaftarPengajuan = async () => {
     try {
       const token = sessionStorage.getItem("token");
@@ -18,6 +22,7 @@ export default function ModalDaftarPengajuan() {
 
       if (data.data) {
         console.log(data.data);
+        setRecap(data.data);
       }
     } catch (error) {
       console.log(error);
@@ -34,9 +39,102 @@ export default function ModalDaftarPengajuan() {
     }
   };
 
+  const dateFormated = (date) => {
+    let dateString = date;
+    let dateObject = new Date(dateString);
+
+    let hari = convertDayString(dateObject);
+    let tanggal = dateObject.getDate();
+    let bulan = dateObject.getMonth() + 1;
+    let tahun = dateObject.getFullYear();
+
+    let formatWaktu = `${hari} ${tanggal}/${bulan}/${tahun}`;
+    return formatWaktu;
+  };
+
+  const typeFormated = (type) => {
+    let formatedType;
+    switch (type) {
+      case "sakit":
+        formatedType = "Sakit";
+        break;
+      case "leaves":
+        formatedType = "Izin";
+        break;
+      case "cuti":
+        formatedType = "Cuti";
+        break;
+      case "wfh":
+        formatedType = "WFH";
+        break;
+
+      default:
+        formatedType = "";
+        break;
+    }
+    return formatedType;
+  };
+
+  const showRecap = () => {
+    console.log(recap);
+    if (recap) {
+      return recap.map((item) => (
+        <div key={item.id}>
+          <div className="card w-100 m-2">
+            <div className="card-header">
+              <div className="d-flex justify-content-between">
+                <h5>{typeFormated(item.type)}</h5>
+                {item.is_approved ? (
+                  <h5 style={{ color: "lime" }}>Disetujui</h5>
+                ) : (
+                  <div className="d-flex justify-content-start align-items-center">
+                    <BsClockFill
+                      style={{ color: "yellow" }}
+                      className="mb-2 me-2"
+                    />
+                    <h5 style={{ color: "yellow" }}>Belum disetujui</h5>
+                  </div>
+                )}
+              </div>
+            </div>
+            <div className="card-body">
+              <div className="d-flex justify-content-between">
+                <p>
+                  <b>Tanggal mulai :</b> {dateFormated(item.start_date)}
+                </p>
+                {item.images ? (
+                  <div className="surat">
+                    <BsImageFill />{" "}
+                    <a
+                      href={`http://localhost:3000/${item.images}`}
+                      target="_blank"
+                    >
+                      Lihat surat
+                    </a>
+                  </div>
+                ) : (
+                  ""
+                )}
+              </div>
+              <p>
+                <b>Tanggal selesai :</b> {dateFormated(item.end_date)}
+              </p>
+              <p>
+                <b>Catatan :</b> {item.note}
+              </p>
+            </div>
+          </div>
+        </div>
+      ));
+    } else {
+      return <i>Anda belum mengajukan apapun...</i>;
+    }
+  };
+
   useEffect(() => {
     getDaftarPengajuan();
   }, []);
+
   return (
     <>
       <div className="card">
@@ -79,7 +177,7 @@ export default function ModalDaftarPengajuan() {
                 aria-label="Close"
               ></button>
             </div>
-            <div className="modal-body">...</div>
+            <div className="modal-body">{showRecap(recap)}</div>
             <div className="modal-footer">
               <button
                 type="button"
